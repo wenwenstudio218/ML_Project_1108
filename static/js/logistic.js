0f0a5644a2133fcfbe36342ef0f438b92d15c640
cindy_hsiao <wenwenstudio218@gmail.com>
2025-11-10 14:29:13 +0800

    1110-14:29

--- static/js/logistic.js
+++ static/js/logistic.js
@@ -126,12 +126,12 @@ document.addEventListener('DOMContentLoaded', () => {
                         x: {
                             title: { display: true, text: chartData.x_label },
                             beginAtZero: true,
-                            max: 5.5
+                            max: 6.0
                         },
                         y: {
                             title: { display: true, text: chartData.y_label },
                             beginAtZero: true,
-                            max: 5.5
+                            max: 6.0
                         }
                     },
                     plugins: {
